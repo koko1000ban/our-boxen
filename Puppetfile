1875d3b4a2013b8915b67ddf6fd0dbc95e2eb0ae
tabito ohtani <koko1000ban@gmail.com>
2013-10-23 23:58:11 +0900

    Tuned for my pc

--- Puppetfile
+++ Puppetfile
@@ -29,10 +29,36 @@ github "nginx",      "1.4.2"
 github "nodejs",     "3.2.9"
 github "openssl",    "1.0.0"
 github "repository", "2.2.0"
-github "ruby",       "6.3.4"
+github "ruby",       "6.5.0"
 github "stdlib",     "4.1.0", :repo => "puppetlabs/puppetlabs-stdlib"
 github "sudo",       "1.0.0"
 github "xquartz",    "1.1.0"
 
+github "homebrew", "1.4.1"
+
 # Optional/custom modules. There are tons available at
 # https://github.com/boxen.
+
+github "osx",     "1.0.0"
+github "osxfuse", "1.1.0"
+
+github "istatmenus3", "1.0.1"
+github "chrome",   "1.1.0"
+github "mysql", "1.1.5"
+github "sourcetree", "1.0.0"
+github "rubymine", "1.0.3"
+github "evernote", "2.0.4"
+github "iterm2", "1.0.3"
+github "mou", "1.1"
+github "sublime_text_2", "1.1.2"
+github "charles", "1.0.2"
+github "appcleaner", "1.0.0"
+github "pow", "1.0.0"
+
+github "vagrant", "2.0.14"
+github "virtualbox", "1.0.7"
+github "packer", "1.0.3"
+github "keyremap4macbook", "1.0.5"
+github "skype", "1.0.6"
+github "sequel_pro", "1.0.1"
+github "dropbox", "1.1.0"
---
+++ modules/people/manifests/koko1000ban.pp
@@ -0,0 +1,161 @@
+class people::koko1000ban {
+  include stdlib
+  include iterm2::stable
+  include chrome
+  include chrome::canary
+  include mysql
+  include sourcetree
+  include rubymine
+  include evernote
+  include mou
+  include sublime_text_2
+  include charles
+  include appcleaner
+  include vagrant
+  include virtualbox
+  include packer
+  include keyremap4macbook
+  include keyremap4macbook::login_item
+  include skype
+  include sequel_pro
+  include dropbox
+  include pow
+  include istatmenus3
+  include osx
+  include osxfuse
+
+  package {
+      [
+        'tmux',
+        'ack',
+        'autoconf',
+        'automake',
+        'cmake',
+        'cmigemo',
+        'coreutils',
+        'ctags',
+        'dmd',
+        'gettext',
+        'git',
+        'global',
+        'gnu-tar',
+        'gnutls',
+        'go',
+        'htop-osx',
+        'jq',
+        'jsonpp',
+        'keychain',
+        'libtool',
+        'libxml2',
+        'libxslt',
+        'libyaml',
+        'lv',
+        'lynx',
+        'moreutils',
+        'ngrep',
+        'nkf',
+        'openssl',
+        'parallel',
+        'pcre',
+        'percona-toolkit',
+        'pkg-config',
+        'readline',
+        'reattach-to-user-namespace',
+        'redis',
+        'rsync',
+        's3sync',
+        'sqlite',
+        'sshfs',
+        'tcpflow',
+        'the_silver_searcher',
+        'tree',
+        'varnish',
+        'watch',
+        'wget',
+        'wireshark',
+        'xz',
+        'z',
+        'zlib'
+      ]:
+    }
+
+    package {
+      'zsh':
+        install_options => [
+          '--disable-etcdir'
+        ]
+    }
+
+    file_line { 'add zsh to /etc/shells':
+      path    => '/etc/shells',
+      line    => "${boxen::config::homebrewdir}/bin/zsh",
+      require => Package['zsh'],
+      before  => Osx_chsh[$::luser];
+    }
+
+    osx_chsh { $::luser:
+      shell   => "${boxen::config::homebrewdir}/bin/zsh";
+    }
+
+    package {
+      'GoogleJapaneseInput':
+        source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+        provider => pkgdmg;
+      'Kobito':
+          source   => "http://kobito.qiita.com/download/Kobito_v1.2.0.zip",
+          provider => compressed_app;
+      'XtraFinder':
+          source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
+          provider => pkgdmg;
+      'Shiftit':
+        source => "https://github.com/downloads/fikovnik/ShiftIt/ShiftIt-1.5.zip",
+        provider => compressed_app;
+    }
+
+    $home     = "/Users/${::luser}"
+    $src      = "${home}/src"
+    $dotfiles = "${src}/dotfiles"
+
+    # ~/src/dotfilesにGitHub上のdotfilesリポジトリを
+    # git-cloneする。そのとき~/srcディレクトリがなければいけない。
+    repository { $dotfiles:
+      source  => "koko1000ban/dotfiles",
+      require => File[$src]
+    }
+    # git-cloneしたらインストールする
+    exec { "sh ${dotfiles}/setup.sh":
+      cwd => $dotfiles,
+      creates => "${home}/.zshrc",
+      require => Repository[$dotfiles],
+    }
+
+    $sublimetext2_packages = "${src}/sublimetext2-packages"
+    repository { $sublimetext2_packages:
+      source  => "koko1000ban/sublimetext2_packages",
+      require => File[$src]
+    }
+
+    exec { "ruby ${sublimetext2_packages}/symlink.rb":
+      cwd => $sublimetext2_packages,
+      creates => "${home}/Library/Application Support/Sublime Text 2/Installed Packages/Package Control.sublime-package",
+      require => Repository[$sublimetext2_packages],
+    }
+
+    keyremap4macbook::set{
+      'repeat.initial_wait':
+        value => '50';
+      'repeat.wait':
+        value => '60';
+      'repeat.consumer_initial_wait':
+        value => '50';
+      'repeat.consumer_wait':
+        value => '60';
+      'repeat.keyoverlaidmodifier_initial_wait':
+        value  => '50';
+    }
+
+    vagrant::plugin { 'aws' }
+    vagrant::plugin { 'vbox-snapshot' }
+
+
+}
